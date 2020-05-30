8c7c4e4974ba3492d8a108cff0d945f85e83ea72
hiroyuki <neginattofan@yahoo.co.jp>
2020-05-30 19:27:28 +0900

    new laravel project

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'haikishare-applicatin.herokuapp.com'),
 
     // 'url' => env('APP_URL', 'http://localhost'),
 
