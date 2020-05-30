82ba4e450d820d6f10cab329b45ee7c2ef8bc45e
hiroyuki <neginattofan@yahoo.co.jp>
2020-05-30 19:15:51 +0900

    new laravel project

--- app/Notifications/CompanyPasswordResetNotification.php
+++ app/Notifications/CompanyPasswordResetNotification.php
@@ -23,7 +23,7 @@ class CompanyPasswordResetNotification extends ResetPassword
         return (new MailMessage)
             ->subject(Lang::getFromJson('Reset Password Notification'))
             ->line(Lang::getFromJson('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::getFromJson('Reset Password'), url(config('app.url').route('company_auth.password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
+            ->action(Lang::getFromJson('Reset Password'), url('haikishare-application.herokuapp.com'.route('company_auth.password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
             ->line(Lang::getFromJson('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
             ->line(Lang::getFromJson('If you did not request a password reset, no further action is required.'));
     }
