f9bf4280f4bc679047b1d441117642d590fc83d7
hiroyuki <neginattofan@yahoo.co.jp>
2020-05-30 20:43:32 +0900

    new laravel project

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'haiki share'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    // 'url' => env('APP_URL', ''),
+    'url' => env('APP_URL', ''),
 
-    'url' => env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
