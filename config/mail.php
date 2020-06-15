24bcf9b08a5e933e1d4083466bf83baed34638ab
hiroyuki <neginattofan@yahoo.co.jp>
2020-06-15 13:43:16 +0900

    config/mail 本番環境メール送信機能gmail対応

--- config/mail.php
+++ config/mail.php
@@ -138,7 +138,7 @@ return [
 
   'driver' => env('MAIL_DRIVER', 'smtp'),
   'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-  'port' => env('MAIL_PORT', 25),
+  'port' => env('MAIL_PORT', 465),
 
   'from' => [
   //ここの設定を追加
